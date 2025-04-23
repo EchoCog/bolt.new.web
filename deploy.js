367ca341728030b8bfe1ba7a0448959d489b5b86
marduk <marduk@rzo.io>
2025-04-23 14:24:48 +0000

    fghht

--- deploy.js
+++ deploy.js
@@ -1,6 +1,31 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
 
-const target = process.argv[2] === '--target=gcloud' ? 'gcloud' : 'cloudflare';
+const target = process.argv.includes('--target=gcloud') ? 'gcloud' : 'cloudflare';
+
+const integrationsPath = '.idx/integrations.json';
+const cloudbuildPath = 'cloudbuild.yaml';
+
+let projectId = '';
+let region = '';
+
+if (fs.existsSync(integrationsPath)) {
+    const integrationsData = JSON.parse(fs.readFileSync(integrationsPath, 'utf-8'));
+    projectId = process.argv.includes('--project-id') ? process.argv[process.argv.indexOf('--project-id') + 1] : integrationsData.cloud_run_deploy.config.projectId;
+    region = process.argv.includes('--region') ? process.argv[process.argv.indexOf('--region') + 1] : integrationsData.cloud_run_deploy.config.region;
+}
+
+if (!projectId || !region){
+    console.error("Error: projectId and region are needed");
+    process.exit(1);
+}
+
+if (fs.existsSync(cloudbuildPath)){
+    let cloudbuildData = fs.readFileSync(cloudbuildPath, 'utf-8');
+    cloudbuildData = cloudbuildData.replace(/\$PROJECT_ID/g, projectId);
+    cloudbuildData = cloudbuildData.replace(/us-central1/g, region);
+    fs.writeFileSync(cloudbuildPath, cloudbuildData);
+}
 
 if (target === 'gcloud') {
   console.log('Deploying to Google Cloud...');
@@ -8,14 +33,23 @@ if (target === 'gcloud') {
     execSync(`gcloud builds submit --config cloudbuild.yaml`, { stdio: 'inherit' });
     console.log('Deployed successfully to Google Cloud');
   } catch (error) {
-    console.error(`Error deploying to Google Cloud: ${error}`);
+    console.error(`Error deploying to Google Cloud:`);
+    if (error.stderr) {
+      console.error(error.stderr.toString());
+    }
+    if (error.stdout){
+        console.log(error.stdout.toString())
+    }
+    process.exit(1);
   }
 } else {
   console.log('Deploying to Cloudflare...');
   try {
-    execSync(`wrangler deploy`, { stdio: 'inherit' });
+    execSync(`pnpm wrangler deploy`, { stdio: 'inherit'});
     console.log('Deployed successfully to Cloudflare');
   } catch (error) {
-    console.error(`Error deploying to Cloudflare: ${error}`);
+    console.error(`Error deploying to Cloudflare:`);
+    console.error(error.stderr.toString())
+    process.exit(1);
   }
 }
